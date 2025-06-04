4b3fa4382d97c8c8eebf386de2a79e6e80f21320
tmdhng7 <robinhong0420@gmail.com>
2025-06-04 23:10:29 +0900

    Update sortlost.py

--- 컴과프 수정/sortlost.py
+++ 컴과프 수정/sortlost.py
@@ -1,22 +1,13 @@
 #정렬기능 +
-#다시 불러오기 기능
-
-from data import lost_item_list, found_item_list, sorted_i_l, sorted_i_f
+#다시 불러오기 기능_f
 
 #정렬 함수
-def sort_seq(var_sort,lost_item_list,found_item_list):
-    for lost in lost_item_list[:]:
-        if lost.state == 1:
-            lost_item_list.remove(lost)
-            found_item_list.append(lost)
+def sort_seq(var_sort,item_list):
     typ = var_sort.get()
     if typ == 'u':
-        sorted_i_l[:] = lost_item_list[::-1]
-        sorted_i_f[:] = found_item_list[::-1]
+        sorted_i = item_list[::-1]
     elif typ == 't':
-        sorted_i_l[:] = sorted(lost_item_list, key=lambda x: x.time, reverse=True)
-        sorted_i_f[:] = sorted(found_item_list, key=lambda x: x.time, reverse=True)
+        sorted_i = sorted(item_list, key=lambda x: x.time, reverse=True)
     elif typ == 'l':
-        sorted_i_l[:] = sorted(lost_item_list, key=lambda x: x.loc)
-        sorted_i_f[:] = sorted(found_item_list, key=lambda x: x.loc)
-    return sorted_i_l, sorted_i_f
+        sorted_i = sorted(item_list, key=lambda x: x.loc)
+    return sorted_i
