196fe8a9dfde55ad14cd2ec848254e1b6b84fd53
tmdhng7 <robinhong0420@gmail.com>
2025-06-05 16:57:32 +0900

    Update sortlost.py

--- 컴과프 수정/sortlost.py
+++ 컴과프 수정/sortlost.py
@@ -2,8 +2,8 @@
 #다시 불러오기 기능_f
 
 #정렬 함수
-def sort_seq(var_sort,item_list):
-    typ = var_sort.get()
+def sort_seq(typ,item_list):
+    sorted_i=[]
     if typ == 'u':
         sorted_i = item_list[::-1]
     elif typ == 't':
