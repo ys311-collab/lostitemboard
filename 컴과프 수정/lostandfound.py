07f6f14b4e3a62884f9324668d528159627db645
tmdhng7 <robinhong0420@gmail.com>
2025-06-05 21:00:30 +0900

    Update lostandfound.py

--- 컴과프 수정/lostandfound.py
+++ 컴과프 수정/lostandfound.py
@@ -2,9 +2,11 @@
 import tkinter as tk
 
 from fileio import load_data, save_data
-import lost
+from lost import Lost
+from cag import LostCAG #Lost ComeAndGet, 잃어버린 것 같은 물건 관리
 from sortlost import sort_seq
 from searchlost import search
+from images import select_image
 
 def start():
     
@@ -21,14 +23,16 @@ def start():
                 found_item_list.remove(inst)
                 lost_item_list.append(inst)
         if type=='lf':    
-            for lost_inst in lost_item_list+found_item_list:
+            for lost_inst in lost_item_list+found_item_list+cag_item_list:
                 lost_inst.trigger.trace_add("write",reload_data_prime)
 
             for w in board_lost_ctxt.winfo_children(): w.destroy()
             for w in board_found_ctxt.winfo_children(): w.destroy()
+            for w in board_cag_ctxt.winfo_children(): w.destroy()
             
             sorted_i_l=sort_seq(var_sort.get(),lost_item_list)[:]
             sorted_i_f=sort_seq(var_sort.get(),found_item_list)[:]
+            sorted_i_cag=sort_seq('u',cag_item_list)
 
             for n, inst in enumerate(sorted_i_l):
                 inst.showState(board_lost_ctxt)
@@ -36,11 +40,15 @@ def start():
             for n, inst in enumerate(sorted_i_f):
                 inst.showState(board_found_ctxt)
                 inst.frm.grid(row=n//4, column=n%4)
+            for n, inst in enumerate(sorted_i_cag):
+                inst.showState(board_cag_ctxt)
+                inst.frm.grid(row=n//4, column=n%4)
             board_lost.pack(side="left", padx=2, pady=2, fill="both", expand=True)
             board_found.pack(side="left", padx=2, pady=2, fill="both", expand=True)
+            board_cag.pack(side="left", padx=2, pady=2, fill="both", expand=True)
             
         elif type=='s':
-            global searched_list #######
+            global searched_list
             sorted_i_s=sort_seq(var_sort.get(),searched_list)[:]
             for w in board_search_ctxt.winfo_children(): w.destroy()
             if not sorted_i_s:
@@ -65,34 +73,66 @@ def start():
     def reload_data_prime(*args): reload_data(typ)
 
     ##입력력
-    def lost_input(ml):
-        def select_image():
-            from tkinter import filedialog
-            global img_path ##########3
-            img_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png *.jpg *.jpeg *.gif")])
+    def lost_input(ml): 
         window = tk.Toplevel(ml)
         window.title("분실물 입력하기")
-        name_et, time_et, loc_et = tk.Entry(window), tk.Entry(window), tk.Entry(window)
-        tk.Label(window, text="이름:").pack(); name_et.pack()
-        tk.Label(window, text="시간(월.일.시:분):").pack(); time_et.pack()
-        tk.Label(window, text="위치:").pack(); loc_et.pack()
-        tk.Button(window, text="사진 선택", command=select_image).pack()
+        frm=tk.Frame(window)
 
-        def assign():
-            lost_item = lost.Lost(name_et.get(), time_et.get(), loc_et.get(), img_path, ml)
-            lost_item_list.append(lost_item)
-            save_data()
-            reload_data(type=typ)
-            window.destroy()
+        def inputLostAndFound(frm):
+            def select_image_int():
+                global img_path
+                img_path=select_image()
 
-        tk.Button(window, text='등록', command=assign).pack()
+            def assign():
+                lost_item = Lost(name_et.get(), time_et.get(), loc_et.get(), img_path, ml)
+                lost_item_list.append(lost_item)
+                reload_data(type=typ)
+                window.destroy()
+
+            name_et, time_et, loc_et = tk.Entry(frm), tk.Entry(frm), tk.Entry(frm)
+            tk.Label(frm, text="이름:").pack(); name_et.pack()
+            tk.Label(frm, text="시간(월.일.시:분):").pack(); time_et.pack()
+            tk.Label(frm, text="위치:").pack(); loc_et.pack()
+            tk.Button(frm, text="사진 선택", command=select_image_int).pack()
+            tk.Button(frm, text='등록', command=assign).pack()
     
+        def inputComeAndFind(frm):
+            def select_image_int():
+                global img_path
+                img_path=select_image()
+
+            def assign():
+                lost_item = LostCAG(name_et.get(), loc_et.get(), char_txt.get("1.0", "end-1c"), img_path, ml)
+                cag_item_list.append(lost_item)
+                reload_data(type=typ)
+                window.destroy()
+
+            name_et,loc_et,char_txt=tk.Entry(frm),tk.Entry(frm),tk.Text(frm, width=20, height=9)
+            tk.Label(frm, text="이름:").pack(); name_et.pack()
+            tk.Label(frm, text="위치:").pack(); loc_et.pack()
+            tk.Label(frm, text="특징 (태그 포함):\n").pack(); char_txt.pack()
+            tk.Button(frm, text="사진 선택", command=select_image_int).pack()
+            tk.Button(frm, text='등록', command=assign).pack()
+
+        def load(typ,frm):
+            for w in frm.winfo_children():
+                w.destroy()
+            if typ=='laf': inputLostAndFound(frm)
+            elif typ=='cag': inputComeAndFind(frm)
+            frm.pack()
+
+        button_bar=tk.Frame(window)
+        tk.Button(button_bar,text='찾아주세요!!',width=10, height=4, bd=0,padx=0,pady=0, command=lambda: load('laf',frm)).pack(side=tk.LEFT)
+        tk.Button(button_bar,text='찾아가세요!!',width=10, height=4, bd=0,padx=0,pady=0, command=lambda: load('cag',frm)).pack(side=tk.LEFT)
+        button_bar.pack(padx=0,pady=0)
+
     ml = tk.Tk()
     ml.title("Lost and Found")
     lost_item_list=[]
     found_item_list=[]
     searched_list=[]
     user_list=[]
+    cag_item_list=[]
     load_data()
 
     #등록하기 : datainput 과 연결
@@ -105,7 +145,7 @@ def start():
     def search_int():
         global searched_list ###33
         global typ #######
-        searched_list=search(lost_item_list+found_item_list, search_et.get().strip())[:]
+        searched_list=search(lost_item_list+found_item_list+cag_item_list, search_et.get().strip())[:]
         typ='s'
         reload_data('s')
 
@@ -140,23 +180,28 @@ def start():
     board=tk.Frame(ml)
     board_lost = tk.Frame(board)
     board_found = tk.Frame(board)
+    board_cag = tk.Frame(board)
     board_search=tk.Frame(board)
     board_mypage=tk.Frame(board)
     tk.Label(board_lost, text='LOST', font=('Arial', 20, 'bold')).pack()
     tk.Label(board_found, text='FOUND', font=('Arial', 20, 'bold')).pack()
+    tk.Label(board_cag, text='COME&GET', font=('Arial', 20, 'bold')).pack()
     tk.Label(board_search,text='SEARCHED', font=('Arial', 20, 'bold')).pack()
     tk.Label(board_mypage, text='MYPAGE', font=('Arial', 20, 'bold')).pack()
 
     board_lost_ctxt = tk.Frame(board_lost)
     board_found_ctxt = tk.Frame(board_found)
+    board_cag_ctxt = tk.Frame(board_cag)
     board_search_ctxt=tk.Frame(board_search)
     board_mypage_ctxt=tk.Frame(board_mypage)
     board_lost.pack(side="left", padx=2, pady=2, fill="both", expand=True)
     board_found.pack(side="left", padx=2, pady=2, fill="both", expand=True)
+    board_cag.pack(side="left", padx=2, pady=2, fill="both", expand=True)
     board_search.pack(side='left')
     board_mypage.pack(side='left')
     board_lost_ctxt.pack()
     board_found_ctxt.pack()
+    board_cag_ctxt.pack()
     board_search_ctxt.pack()
     board_mypage_ctxt.pack()
 
