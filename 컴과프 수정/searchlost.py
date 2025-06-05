27bbadbfc77b0336dca4e12ed62aa479bf3da67c
tmdhng7 <robinhong0420@gmail.com>
2025-06-05 14:20:06 +0900

    Create searchlost.py

---
+++ 컴과프 수정/searchlost.py
@@ -0,0 +1,12 @@
+from str_dist import str_dist
+from abrev_check import is_abrev_both
+
+def search(data, search_str): #data는 리스트형, 내부에 Lost클래스
+    searched_li=[]
+    for inst in data:
+        for data_piece in inst.data:
+            if isinstance(data_piece,str):
+                if str_dist(inp=search_str,ans=data_piece)<=2 or is_abrev_both(data_piece,search_str):
+                    searched_li.append(inst)
+                    break
+    return searched_li[:]
