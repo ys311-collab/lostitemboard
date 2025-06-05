3382e2d822d98941dbf3b89af53a2099c63185ba
tmdhng7 <robinhong0420@gmail.com>
2025-06-05 21:00:44 +0900

    Update searchlost.py

--- 컴과프 수정/searchlost.py
+++ 컴과프 수정/searchlost.py
@@ -9,4 +9,9 @@ def search(data, search_str): #data는 리스트형, 내부에 Lost클래스
                 if str_dist(inp=search_str,ans=data_piece)<=2 or is_abrev_both(data_piece,search_str):
                     searched_li.append(inst)
                     break
+            if isinstance(data_piece,list):
+                for val in data_piece:
+                    if str_dist(inp=search_str,ans=data_piece)<=2 or is_abrev_both(data_piece,search_str):
+                        searched_li.append(inst)
+                        break
     return searched_li[:]
