58ed7f7403153b5ac6443a1e72295020374eb0e1
tmdhng7 <robinhong0420@gmail.com>
2025-06-05 14:18:53 +0900

    Update images.py

--- 컴과프 수정/images.py
+++ 컴과프 수정/images.py
@@ -1,11 +1,12 @@
 from PIL import Image, ImageTk
 
-def load_image(img_path):
+def load_image(img_path, frame):
     try:
         image = Image.open(img_path)
-        image = image.resize((100, 100))
-        return ImageTk.PhotoImage(image)
-    except:
+        # image = image.resize((100, 100))
+        return ImageTk.PhotoImage(image, master=frame)
+    except Exception as e:
+        print(e)
         return None
     
 def save_image(image,save_path):
