f9f1ca3a7f3fb5f378c57e479fca13a70f5a5dd7
tmdhng7 <robinhong0420@gmail.com>
2025-06-10 04:09:55 +0900

    Update abrev_check.py

--- 컴과프 수정/abrev_check.py
+++ 컴과프 수정/abrev_check.py
@@ -1,8 +1,8 @@
-def is_abrev(full,short):
+def is_abrev(full,short): #short가 full의 준말인지 확인하는 함수
     for ch in short:
         if ch not in full: return False
         full=full[full.find(ch)+1:]
     return True
 
-def is_abrev_both(str1,str2):
+def is_abrev_both(str1,str2): # 한쪽이 다른 쪽의 준말인지 확인하는 함수수
     return is_abrev(str1,str2) or is_abrev(str2,str1)
