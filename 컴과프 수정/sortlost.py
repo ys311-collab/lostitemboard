f15d878a29d7ecf1433c1b7c6ff5a0036d220dea
tmdhng7 <robinhong0420@gmail.com>
2025-06-10 04:11:07 +0900

    Update sortlost.py

--- 컴과프 수정/sortlost.py
+++ 컴과프 수정/sortlost.py
@@ -2,12 +2,30 @@
 #다시 불러오기 기능_f
 
 #정렬 함수
-def sort_seq(typ,item_list):
+def sort_seq_l(typ,item_list):
     sorted_i=[]
-    if typ == 'u':
+    if typ == 'u': #업로드 날짜 순 (최근일 수록 앞)
         sorted_i = item_list[::-1]
-    elif typ == 't':
+    elif typ == 't': #잃어버렸다 생각되는 시간 순 (최근일 수록 앞)
         sorted_i = sorted(item_list, key=lambda x: x.time, reverse=True)
-    elif typ == 'l':
+    elif typ == 'l': #위치의 ㄱㄴㄷ순
+        sorted_i = sorted(item_list, key=lambda x: x.loc)
+    return sorted_i
+
+def sort_seq_f(typ,item_list):
+    sorted_i=[]
+    if typ == 'u': #업로드 날짜 순
+        sorted_i = item_list[::-1]
+    elif typ == 't': # 찾아준 날짜 순
+        sorted_i = sorted(item_list, key=lambda x: x.find_time, reverse=True)
+    elif typ == 'l': # 찾은 위치의 ㄱㄴㄷ순
+        sorted_i = sorted(item_list, key=lambda x: x.find_loc)
+    return sorted_i
+
+def sort_seq_cag(typ,item_list):
+    sorted_i=[]
+    if typ == 'u' or typ=='t': #업로드 순
+        sorted_i = item_list[::-1]
+    elif typ == 'l': #장소 순순
         sorted_i = sorted(item_list, key=lambda x: x.loc)
     return sorted_i
