bf3a0cd8e4501acffea5f8efc7e70613f478f72f
tmdhng7 <robinhong0420@gmail.com>
2025-06-10 04:11:25 +0900

    Update str_dist.py

--- 컴과프 수정/str_dist.py
+++ 컴과프 수정/str_dist.py
@@ -1,13 +1,13 @@
 from jamo import h2j
 
 def str_dist(inp,ans):
-    def is_hangeul(char): # 한글인지 판단하는 모듈
+    def is_hangeul(char): # 한글인지 판단
         return (
-            ('가' <= char <= '힣') or  # 한글
-            ('ㄱ' <= char <= 'ㅎ') or  # 자음
-            ('ㅏ' <= char <= 'ㅣ')     # 모음
+            ('가' <= char <= '힣') or  
+            ('ㄱ' <= char <= 'ㅎ') or  
+            ('ㅏ' <= char <= 'ㅣ')     
         )
-    def conv_str(string): # 한글->자모 영어->소문자
+    def conv_str(string): # 한글->자모 / 영어->소문자
         res=''
         for ch in string:
             if is_hangeul(ch): res+=h2j(ch)
@@ -17,14 +17,15 @@ def str_dist(inp,ans):
 
     def find_closest(inp,ans,inp_idx,ans_idx):
         min_dist_list=[max((len(ans)-1-ans_idx),(len(inp)-1-inp_idx)),len(inp),len(ans)]
-        for idx in range(inp_idx+1,len(inp)):
+        for idx in range(inp_idx+1,len(inp)): #inp의 idx>inp_idx 인덱스 값과 ans의 next_find>ans_idx 인덱시 값이 같은
+            #것들 중에서 그 차이의 합이 최소가 되도록 함. 없다면 inp_idx, ans_idx 값을 가짐.
             if ans_idx<len(ans)-1 and inp[idx] in ans[ans_idx+1:]:
                 next_find=ans[ans_idx+1:].find(inp[inp_idx])
             else:
                 next_find=len(ans)
             dist=max(next_find-ans_idx, idx-inp_idx)
             if min_dist_list[0]>dist:
-                min_dist_list=[dist,next_find,idx]
+                min_dist_list=[dist,next_find,idx] #거리, answer의 리더 인덱스, input의 리더 인덱스. 둘의 리스트에서의 값은 같다.
         return min_dist_list
 
     inp,ans = conv_str(inp),conv_str(ans) #변환 완료된 문자열 string
@@ -33,14 +34,14 @@ def str_dist(inp,ans):
     inp_rd, ans_rd = 0,0 # 읽고 있는 인덱스 위치
     try:
         while inp_rd<=len(inp)-1 and ans_rd<=len(ans)-1:
-            if inp[inp_rd]==ans[ans_rd]:
+            if inp[inp_rd]==ans[ans_rd]: #같으면 다음 칸으로 이동해 읽기
                 inp_rd+=1
                 ans_rd+=1
             else: 
-                #오타 발생 !!
+                #오타 발생 !! 두 값이 같은 최소 거리의 인덱스로 리더를 이동시킴
                 distance_list=find_closest(inp,ans,inp_rd,ans_rd)
                 dist+=distance_list[0]
                 ans_rd,inp_rd= distance_list[1],distance_list[2]
     except Exception as e:
-        print(e)
+        pass
     return dist
