84481d6e073d960ed8f6a08d277be192c49d0436
tmdhng7 <robinhong0420@gmail.com>
2025-06-10 03:16:08 +0900

    Update images.py

--- 컴과프 수정/images.py
+++ 컴과프 수정/images.py
@@ -6,7 +6,6 @@ def load_image(img_path, frame):
         # image = image.resize((100, 100))
         return ImageTk.PhotoImage(image, master=frame)
     except Exception as e:
-        print(e)
         return None
     
 def save_image(image,save_path):
