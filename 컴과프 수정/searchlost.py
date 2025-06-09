fab5e79cfbb3f7b32d80391c9f401ee6b6623716
tmdhng7 <robinhong0420@gmail.com>
2025-06-10 04:09:28 +0900

    Update searchlost.py

--- 컴과프 수정/searchlost.py
+++ 컴과프 수정/searchlost.py
@@ -1,19 +1,19 @@
 from str_dist import str_dist # 문자열 간 거리 계산 함수
-from abrev_check import is_abrev_both
+from abrev_check import is_abrev_both # 문자열 간 준말 관계 확인
 
-def search(datas, search_str): #data는 리스트형, 내부에 Lost클래스
-    searched_li=[]
-    for inst in datas:
+def search(datas, search_str): #datas는 리스트형, 내부에 Lost클래스
+    searched_li=[] #검색 결과
+    for inst in datas: 
         for data_piece in inst.data:
-            if isinstance(data_piece,str):
-                if str_dist(inp=search_str,ans=data_piece)<=2 or is_abrev_both(data_piece,search_str):
+            if isinstance(data_piece,str): #str형 데이터에서 비교
+                if str_dist(inp=search_str,ans=data_piece)<=2 or is_abrev_both(data_piece,search_str): #오차거리 2 허용
                     searched_li.append(inst)
                     break
-            if isinstance(data_piece,list):
+            elif isinstance(data_piece,list): #list형 데이터에서
                 for val in data_piece:
-                    if isinstance(val,str):
+                    if isinstance(val,str): #str형 내부 객체를 비교
                         if str_dist(inp=search_str,ans=val)<=2 or is_abrev_both(val,search_str):
                             searched_li.append(inst)
                             break
-            if inst in searched_li: break
+            if inst in searched_li: break #중복 검색 안 되도록 탈출
     return searched_li[:]
