5619377710eba8e5c72f4cf961f482d7575314e3
ys311-collab <danielleys31@gmail.com>
2025-06-10 03:29:58 +0900

    Update images.py

--- 컴과프 수정/images.py
+++ 컴과프 수정/images.py
@@ -1,16 +1,16 @@
 from PIL import Image, ImageTk
 
+#이미지 경로와 프레임을 받았을 때 이미지를 표시해주는 함수
 def load_image(img_path, frame):
     try:
         image = Image.open(img_path)
-        # image = image.resize((100, 100))
+        image = image.resize((100, 100))  #이미지 크기 조정
         return ImageTk.PhotoImage(image, master=frame)
-    except Exception as e:
+    except Exception as e:  #예외 처리
+        print(e)
         return None
-    
-def save_image(image,save_path):
-    image.save(save_path)
 
+#이미지를 컴퓨터 파일에서 선택하게 하는 함수수
 def select_image():
     from tkinter import filedialog
     img_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png *.jpg *.jpeg *.gif")])
